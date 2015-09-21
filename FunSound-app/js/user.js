79cdbfdf21f261accc0d125ffb915e6513f61b2a
Alberto <alberto.perez.calleja@gmail.com>
2015-09-21 16:22:14 +0200

    remember me button featura implementado

--- FunSound-app/js/user.js
+++ FunSound-app/js/user.js
@@ -5,9 +5,14 @@ Todo:
 --*/
 
 function user_validate(user,pass){
-	if (document.getElementById('remember_me').checked )
+	//handle with remmeber me app feature
+	var remember_box = document.getElementById('remember_me');
+	
+	if ( remember_box.checked )
 		localStorage.setItem('app.remember_me', 1);
 	else localStorage.setItem('app.remember_me', 0);
+	
+	//
 	
 	return 1;
 	
